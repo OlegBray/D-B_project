64d66b42fa8d48795de080b5535e2dc7a137277b
Oleg, Braylovskiy <oleghoog123@gmail.com>
2025-07-01 16:40:27 +0300

    Created terraform module structure

---
+++ terraform/var.tf
@@ -0,0 +1,38 @@
+variable "region" {
+  default = "eu-central-1"
+}
+
+variable "vpc_cidr" {
+  default = "172.10.0.0/16"
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["172.10.100.0/24", "172.10.110.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["172.10.20.0/24", "172.10.30.0/24"]
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["eu-central-1a", "eu-central-1b"]
+}
+
+variable "ami_id" {
+  default = "ami-02003f9f0fde924ea"
+}
+
+variable "instance_type" {
+  default = "t2.medium"
+}
+
+variable "public_instance_count" {
+  default = 1
+}
+
+variable "private_instance_count" {
+  default = 3
+}
