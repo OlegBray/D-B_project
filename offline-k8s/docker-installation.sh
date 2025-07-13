cc386af747d32faa46d3b04324017f6f7db78350
Oleg, Braylovskiy <oleghoog123@gmail.com>
2025-07-13 16:48:54 +0300

    Did a reset to commit be58a42

--- offline-k8s/docker-installation.sh
+++ offline-k8s/docker-installation.sh
@@ -3,3 +3,9 @@ cd ~/offline-k8s/docker
 apt download docker.io
 apt download containerd
 apt download runc
+
+mkdir -p ~/offline-k8s/images
+cd ~/offline-k8s/images
+docker pull registry:2 && docker save registry:2 -o registry.tar
+
+
